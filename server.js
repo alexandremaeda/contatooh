0768a55d63bc5b410fe96a14872d78505042b891
Alexandre Maeda <ale.b.maeda@gmail.com>
2015-08-23 10:15:20 -0300

    Correção port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var app = require('./config/express')();
 require('./config/passport')();
-require('./config/database.js')('mongodb://localhost/contatooh');
+require('./config/database.js')('mongodb://admin:admin**@ds035653.mongolab.com:35653/contatooh');
 
-http.createServer(app).listen(process.env.port || 3000, function(){
+app.set('port', (process.env.PORT || 3000));
+
+http.createServer(app).listen(app.get('port'), function(){
 	console.log('Server online na porta ' + app.get('port'));
 });
