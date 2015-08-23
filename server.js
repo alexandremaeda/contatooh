3c70fef7615b52350058a7340d66522d132f5653
Alexandre Maeda <ale.b.maeda@gmail.com>
2015-08-23 10:04:21 -0300

    port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./config/express')();
 require('./config/passport')();
 require('./config/database.js')('mongodb://localhost/contatooh');
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(process.env.port || 3000, function(){
 	console.log('Server online na porta ' + app.get('port'));
 });
