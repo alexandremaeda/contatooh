2d6004ebd7b08ba0455d4f7575684a991ddf6bc3
Alexandre Maeda <ale.b.maeda@gmail.com>
2015-08-23 10:37:46 -0300

    format

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,38 +1,37 @@
 angular.module('contatooh').controller('ContatosController', ContatosController);
 
-function ContatosController($scope, ContatoService){
-	$scope.total = 0;
-	$scope.busca = '';
-	$scope.contatos = [];
-	$scope.erro = {
-		mensagem: null
-	};
+function ContatosController($scope, ContatoService) {
+    $scope.total = 0;
+    $scope.busca = '';
+    $scope.contatos = [];
+    $scope.erro = {
+        mensagem: null
+    };
 
-	function buscaContatos(){
-		ContatoService.query(
-			function(data){
-				$scope.contatos = data;
-			},
-			function(erro){
-				console.log(erro);
-				$scope.erro.mensagem = 'Não possível obter os Contatos.';
-			}
-		);
-	}
+    function buscaContatos() {
+        ContatoService.query(
+            function(data) {
+                $scope.contatos = data;
+            },
+            function(erro) {
+                console.log(erro);
+                $scope.erro.mensagem = 'Não possível obter os Contatos.';
+            }
+        );
+    }
 
-	$scope.excluir = function(contato){
-		ContatoService.delete({
-			id: contato._id
-		}, buscaContatos
-		, function(erro){
-			console.log(erro);
-			$scope.erro.mensagem = 'Não possível excluir ' + contato.nome + '.';
-		});
-	};
+    $scope.excluir = function(contato) {
+        ContatoService.delete({
+            id: contato._id
+        }, buscaContatos, function(erro) {
+            console.log(erro);
+            $scope.erro.mensagem = 'Não possível excluir ' + contato.nome + '.';
+        });
+    };
 
-	$scope.incrementa = function(){
-		$scope.total++;
-	}
+    $scope.incrementa = function() {
+        $scope.total++;
+    }
 
-	buscaContatos();
+    buscaContatos();
 }
