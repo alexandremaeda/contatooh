f7b66540cdce704d11a7f9b84afd840d8401fff9
Alexandre Maeda <ale.b.maeda@gmail.com>
2015-08-24 14:32:21 -0300

    like good

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,7 +1,20 @@
 angular.module('contatooh').controller('HomeController', HomeController);
 
-function HomeController() {
+function HomeController($rootScope) {
     var vm = this;
 
-    vm.url = 'http://teste.com';
+    $rootScope.$on('$routeChangeSuccess', function(e, current, pre){
+        if (current.$$route.originalPath == '/'){
+            vm.url = 'https://alexandremaeda.herokuapp.com/#/';
+            vm.title = 'Home';
+            vm.discription = 'Página principal';
+        }
+        else if(current.$$route.originalPath == '/contato'){
+            vm.url = 'https://alexandremaeda.herokuapp.com/#/contato';
+            vm.title = 'Contato';
+            vm.discription = 'Página Contato';
+        }
+    });
+
+    
 }
